d9f1c6a7afab1c4befd241038688f07e0bdc4329
pave19 <veerupalacherla@gmail.com>
2022-10-28 22:57:00 +0530

    test2

--- src/main/java/StringOperations.java
+++ src/main/java/StringOperations.java
@@ -3,7 +3,8 @@ import java.util.Arrays;
 public class StringOperations {
 
     public static String StringConcatenation(String s1, String s2) {
-
+        String s3 = s1.concat(s2);
+        return s3;
     }
-    return 0;
+
 }
